327f670092d7547b1359b2d807f469ace6b7ba3c
rohitaryal <erucix@gmail.com>
2025-04-07 22:28:12 +0530

    format code

--- src/utils/filemanager.ts
+++ src/utils/filemanager.ts
@@ -1,34 +1,47 @@
 import fs from "fs";
 import path from "path";
 
-const saveFile = (fileName: string, fileContent: string, encoding: BufferEncoding = "utf-8", filePath: string = "."): boolean => {
-  const fullPath = path.join(filePath, fileName);
-  const parsedPath = path.parse(fullPath);
+const saveFile = (
+    fileName: string,
+    fileContent: string,
+    encoding: BufferEncoding = "utf-8",
+    filePath: string = ".",
+): boolean => {
+    const fullPath = path.join(filePath, fileName);
+    const parsedPath = path.parse(fullPath);
 
-  if (parsedPath.dir && !fs.existsSync(parsedPath.dir) && parsedPath.dir != ".") {
-    try {
-      fs.mkdirSync(parsedPath.dir, { recursive: true })
-    } catch (error) {
-      console.log(`[!] Failed to create directory: ${parsedPath.dir}`);
-      console.log(error);
+    if (
+        parsedPath.dir &&
+        !fs.existsSync(parsedPath.dir) &&
+        parsedPath.dir != "."
+    ) {
+        try {
+            fs.mkdirSync(parsedPath.dir, { recursive: true });
+        } catch (error) {
+            console.log(`[!] Failed to create directory: ${parsedPath.dir}`);
+            console.log(error);
 
-      return false;
+            return false;
+        }
     }
-  }
 
-  try {
-    fs.writeFileSync(fullPath, fileContent, { encoding });
-  } catch (error) {
-    console.log(`[!] Failed to write into file.`);
-    console.log(error);
-    return false;
-  }
+    try {
+        fs.writeFileSync(fullPath, fileContent, { encoding });
+    } catch (error) {
+        console.log(`[!] Failed to write into file.`);
+        console.log(error);
+        return false;
+    }
 
-  return true;
-}
+    return true;
+};
 
-const saveImage = (fileName: string, imageContent: string, filePath: string = "."): boolean => {
-  return saveFile(fileName, imageContent, "base64", filePath);
-}
+const saveImage = (
+    fileName: string,
+    imageContent: string,
+    filePath: string = ".",
+): boolean => {
+    return saveFile(fileName, imageContent, "base64", filePath);
+};
 
 export { saveFile, saveImage };
