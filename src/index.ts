df4b0c8075f250775ca148e6afeb455e326f6b9c
rohitaryal <erucix@gmail.com>
2025-04-07 23:09:36 +0530

    added error type

--- src/index.ts
+++ src/index.ts
@@ -22,37 +22,56 @@ interface ModelResponse {
     }[];
 }
 
+interface ErrorResponse {
+    error: {
+        code: number;
+        message: string;
+        status: string;
+    };
+}
+
 const generateImage = ({
     prompt,
     imageCount,
     authorization,
     seed,
 }: GenerateImageProps) => {
-    return new Promise((resolve: (value: ModelResponse) => void, reject) => {
-        request({
-            authorization,
-            method: "POST",
-            reqURL: "https://aisandbox-pa.googleapis.com/v1:runImageFx",
-            body: JSON.stringify({
-                userInput: {
-                    candidatesCount: imageCount,
-                    prompts: [prompt],
-                    seed: seed,
-                },
-                clientContext: {
-                    sessionId: ";1740656431200",
-                    tool: "IMAGE_FX",
-                },
-                modelInput: {
-                    modelNameType: "IMAGEN_3_1",
-                },
-                aspectRatio: "IMAGE_ASPECT_RATIO_LANDSCAPE",
-            }),
-        })
-            .then((response) => response.json())
-            .then((response) => resolve(response))
-            .catch((error) => reject(error));
-    });
+    return new Promise(
+        (
+            resolve: (value: ModelResponse) => void,
+            reject: (value: ErrorResponse) => void,
+        ) => {
+            request({
+                authorization,
+                method: "POST",
+                reqURL: "https://aisandbox-pa.googleapis.com/v1:runImageFx",
+                body: JSON.stringify({
+                    userInput: {
+                        candidatesCount: imageCount,
+                        prompts: [prompt],
+                        seed: seed,
+                    },
+                    clientContext: {
+                        sessionId: ";1740656431200",
+                        tool: "IMAGE_FX",
+                    },
+                    modelInput: {
+                        modelNameType: "IMAGEN_3_1",
+                    },
+                    aspectRatio: "IMAGE_ASPECT_RATIO_LANDSCAPE",
+                }),
+            })
+                .then((response) => response.json())
+                .then((response) => {
+                    if (response.error) {
+                        reject(response as ErrorResponse);
+                    } else {
+                        resolve(response as ModelResponse);
+                    }
+                })
+                .catch((error) => reject(error));
+        },
+    );
 };
 
 export default generateImage;
