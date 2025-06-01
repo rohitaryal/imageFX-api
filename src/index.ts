dfdaf512ced23bacc8da1d481d82e55f9e4ef9d2
rohitaryal <erucix@gmail.com>
2025-06-01 20:17:37 +0530

    change request type

--- src/index.ts
+++ src/index.ts
@@ -1,81 +1,81 @@
 import request from "./utils/request.ts";
 
 interface GenerateImageProps {
-    seed: number | null;
-    prompt: string;
-    imageCount: number;
-    authorization: string;
-    model: "IMAGEN_2" | "IMAGEN_3" | "IMAGEN_3_1" | "IMAGEN_3_5" | "IMAGEN_2_LANDSCAPE" | "IMAGEN_3_PORTRAIT" | "IMAGEN_3_LANDSCAPE" | "IMAGEN_3_PORTRAIT_THREE_FOUR" | "IMAGEN_3_LANDSCAPE_FOUR_THREE" | "IMAGE_MODEL_NAME_UNSPECIFIED";
-    aspectRatio: "IMAGE_ASPECT_RATIO_SQUARE" | "IMAGE_ASPECT_RATIO_PORTRAIT" | "IMAGE_ASPECT_RATIO_LANDSCAPE" | "IMAGE_ASPECT_RATIO_UNSPECIFIED" | "IMAGE_ASPECT_RATIO_LANDSCAPE_FOUR_THREE" | "IMAGE_ASPECT_RATIO_PORTRAIT_THREE_FOUR";
+  seed: number | null;
+  prompt: string;
+  imageCount: number;
+  authorization: string;
+  model: "IMAGEN_2" | "IMAGEN_3" | "IMAGEN_3_1" | "IMAGEN_3_5" | "IMAGEN_2_LANDSCAPE" | "IMAGEN_3_PORTRAIT" | "IMAGEN_3_LANDSCAPE" | "IMAGEN_3_PORTRAIT_THREE_FOUR" | "IMAGEN_3_LANDSCAPE_FOUR_THREE" | "IMAGE_MODEL_NAME_UNSPECIFIED";
+  aspectRatio: "IMAGE_ASPECT_RATIO_SQUARE" | "IMAGE_ASPECT_RATIO_PORTRAIT" | "IMAGE_ASPECT_RATIO_LANDSCAPE" | "IMAGE_ASPECT_RATIO_UNSPECIFIED" | "IMAGE_ASPECT_RATIO_LANDSCAPE_FOUR_THREE" | "IMAGE_ASPECT_RATIO_PORTRAIT_THREE_FOUR";
 }
 
 interface ModelResponse {
-    imagePanels: {
-        prompt: string;
-        generatedImages: {
-            encodedImage: string;
-            seed: number;
-            mediaGenerationId: string;
-            isMaskEditedImage: boolean;
-            modelNameType: string;
-            workflowId: string;
-            fingerprintLogRecordId: string;
-        }[];
+  imagePanels: {
+    prompt: string;
+    generatedImages: {
+      encodedImage: string;
+      seed: number;
+      mediaGenerationId: string;
+      isMaskEditedImage: boolean;
+      modelNameType: string;
+      workflowId: string;
+      fingerprintLogRecordId: string;
     }[];
+  }[];
 }
 
 interface ErrorResponse {
-    error: {
-        code: number;
-        message: string;
-        status: string;
-    };
+  error: {
+    code: number;
+    message: string;
+    status: string;
+  };
 }
 
 const generateImage = ({
-    prompt,
-    imageCount,
-    authorization,
-    seed,
-    model,
-    aspectRatio,
+  prompt,
+  imageCount,
+  authorization,
+  seed,
+  model,
+  aspectRatio,
 }: GenerateImageProps) => {
-    return new Promise(
-        (
-            resolve: (value: ModelResponse) => void,
-            reject: (value: ErrorResponse) => void,
-        ) => {
-            request({
-                authorization,
-                method: "POST",
-                reqURL: "https://aisandbox-pa.googleapis.com/v1:runImageFx",
-                body: JSON.stringify({
-                    userInput: {
-                        candidatesCount: imageCount,
-                        prompts: [prompt],
-                        seed: seed,
-                    },
-                    clientContext: {
-                        sessionId: ";1740658431200",
-                        tool: "IMAGE_FX",
-                    },
-                    modelInput: {
-                        modelNameType: model,
-                    },
-                    aspectRatio: aspectRatio,
-                }),
-            })
-                .then((response) => response.json())
-                .then((response) => {
-                    if (response.error) {
-                        reject(response as ErrorResponse);
-                    } else {
-                        resolve(response as ModelResponse);
-                    }
-                })
-                .catch((error) => reject(error));
-        },
-    );
+  return new Promise(
+    (
+      resolve: (value: ModelResponse) => void,
+      reject: (value: ErrorResponse) => void,
+    ) => {
+      request({
+        authorization,
+        method: "POST",
+        reqURL: "https://aisandbox-pa.googleapis.com/v1:runImageFx",
+        body: JSON.stringify({
+          userInput: {
+            candidatesCount: imageCount,
+            prompts: [prompt],
+            seed: seed,
+          },
+          clientContext: {
+            sessionId: ";1740658431200",
+            tool: "IMAGE_FX",
+          },
+          modelInput: {
+            modelNameType: model,
+          },
+          aspectRatio: aspectRatio,
+        }),
+      })
+        .then((response) => response.json())
+        .then((response) => {
+          if (response.error) {
+            reject(response as ErrorResponse);
+          } else {
+            resolve(response as ModelResponse);
+          }
+        })
+        .catch((error) => reject(error));
+    },
+  );
 };
 
 export default generateImage;
--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,41 +1,33 @@
-interface Options {
-    reqURL: string;
-    authorization: string;
-    method: "GET" | "POST";
-    body?: string;
-}
+import type { Request, Result } from "../global.types";
 
-const defaultHeaders = new Headers({
-    accept: "*/*",
-    "accept-language": "en-US,en;q=0.9",
-    "content-type": "text/plain;charset=UTF-8",
-    dnt: "1",
-    origin: "https://labs.google",
-    priority: "u=1, i",
-    referer: "https://labs.google/",
-    "sec-ch-ua":
-        '"Not(A:Brand";v="99", "Google Chrome";v="133", "Chromium";v="133"',
-    "sec-ch-ua-mobile": "?0",
-    "sec-ch-ua-platform": '"Linux"',
-    "sec-fetch-dest": "empty",
-    "sec-fetch-mode": "cors",
-    "sec-fetch-site": "cross-site",
-    "user-agent":
-        "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/133.0.0.0 Safari/537.36",
-});
+const request = async function (req: Request): Promise<Result<string>> {
+  req.headers.set("Origin", "https://labs.google");
+  req.headers.set("Referer", "https://labs.google");
 
-const request = (options: Options, headers: Headers = defaultHeaders) => {
-    if (!options.authorization.startsWith("Bearer")) {
-        options.authorization = "Bearer " + options.authorization;
+  try {
+    const reqs = await fetch(req.url, {
+      body: req.body,
+      method: req.method,
+      headers: req.headers,
+    });
+
+    const res = await reqs.text();
+
+    if (!reqs.ok) {
+      return {
+        Err: new Error(res)
+      }
     }
 
-    headers.append("authorization", options.authorization);
+    return {
+      Ok: res,
+    };
 
-    return fetch(options.reqURL, {
-        headers,
-        method: options.method,
-        body: options.body,
-    });
-};
+  } catch (err) {
+    return {
+      Err: (err instanceof Error) ? err : new Error("Failed to fetch.")
+    }
+  }
+}
 
-export default request;
+export { request };
