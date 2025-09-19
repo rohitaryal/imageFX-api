08f6dfc51e84bdafe8cc9f9677b97fd4d2642659
rohitaryal <erucix@gmail.com>
2025-09-19 22:40:39 +0530

    Added types for images

--- src/Constants.ts
+++ src/Constants.ts
@@ -18,5 +18,20 @@ export const DefaultHeader = new Headers({
     "Referer": "https://labs.google/fx/tools/image-fx"
 });
 
+export const ImageType = Object.freeze({
+    JPEG: "jpeg",
+    JPG: "jpg",
+    JPE: "jpe",
+    PNG: "png",
+    GIF: "gif",
+    WEBP: "webp",
+    SVG: "svg",
+    BMP: "bmp",
+    TIFF: "tiff",
+    APNG: "apng",
+    AVIF: "avif",
+} as const);
+
 export type Model = typeof Model[keyof typeof Model]
 export type AspectRatio = typeof AspectRatio[keyof typeof AspectRatio]
+export type ImageType = typeof ImageType[keyof typeof ImageType];
