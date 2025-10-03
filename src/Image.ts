f3b1eeee0b9d602d85f959fb267ac3c27ed8d553
rohitaryal <erucix@gmail.com>
2025-10-04 03:37:15 +0530

    Fix: Add comments for image property, function description, etc

--- src/Image.ts
+++ src/Image.ts
@@ -10,15 +10,56 @@ export class ImageError extends Error {
     }
 }
 
+/**
+ * Represents a generated image.
+ */
 export class Image {
+    /**
+     * Represents seed value used for this image generation.
+     * Seed is a specific number that serves as the starting point
+     * for the random process used to create the image.
+     */
     public readonly seed: number;
+    /**
+    * Model used to generate this imge.
+    * 
+    * Note: `"IMAGEN_3_5"` is probably `IMAGEN_4`
+    * 
+    * Available models:
+    * - `"IMAGEN_3"`
+    * - `"IMAGEN_3_1"`
+    * - `"IMAGEN_3_5"`
+    */
     public readonly model: Model; // modelNameType
+
+    /**
+     * Textual description used to generate this image
+     */
     public readonly prompt: string;
+
+    /**
+     * Aspect ratio of this image
+     */
     public readonly aspectRatio: AspectRatio;
+
+    /**
+     * Unique id assigned to each generated image.
+     * 
+     * `mediaId` can be used as identifier to download the image.
+     */
     public readonly mediaId: string; // mediaGenerationId
 
+    /**
+     * Generated `png` image encoded into base64.
+     */
     private readonly encodedImage: string;
+    /**
+     * Project id is what i can guess
+     */
     private readonly workflowId: string;
+    /**
+     * IDK what this is bro
+     */
     private readonly fingerprintId: string; // fingerprintLogRecordId
 
     constructor(args: ImageArg) {
@@ -37,6 +78,12 @@ export class Image {
         this.fingerprintId = args.fingerprintLogRecordId;
     }
 
+    /**
+     * Saves image to specified path with timestamp as name.
+     * Default path is current directory.
+     * 
+     * @param filePath Directory for the image to be saved
+     */
     public save(filePath = ".") {
         const imageName = `image-${Date.now()}.png`;
 
